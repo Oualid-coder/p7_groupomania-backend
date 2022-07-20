1b0b7d39a9342d3415c22d5cd960ab4077b037c7
Oualid <oualid.najah@gmail.com>
2022-07-20 23:46:41 +0200

    retouche

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -5,7 +5,7 @@ const { uploadErrors } = require("../utils/errors.utils");
 const ObjectID = require("mongoose").Types.ObjectId;
 const fs = require("fs");
 const { promisify } = require("util");
-const pipeline = promisify(require("stream").pipeline);
+
 
 module.exports.readPost = (req, res) => {
   PostModel.find((err, docs) => {
@@ -43,49 +43,7 @@ if(req.file !== null){
  
 
 
-  // if (req.file !== null) {
-
-
-    // fileName=req.body.posterId+Date.now()+".jpg"
-    // console.log(fileName)
-  //   try {
-  //     if (
-  //       req.file.detectedMimeType != "image/jpg" &&
-  //       req.file.detectedMimeType != "image/png" &&
-  //       req.file.detectedMimeType != "image/jpeg"
-  //     )
-  //       throw Error("invalid file");
-
-  //     if (req.file.size > 500000) throw Error("max size");
-  //   } catch (err) {
-  //     const errors = uploadErrors(err);
-  //     return res.status(201).json({ errors });
-  //   }
-    // fileName = req.file.filename;
-
-    // await pipeline(
-    //   req.file.stream,
-    //   fs.createWriteStream(
-    //     `${__dirname}/../client/public/uploads/posts/${fileName}`
-    //   )
-    // );
-  // }
-
-  // const newPost = new postModel({
-  //   posterId: req.body.posterId,
-  //   message: req.body.message,
-  //   picture: req.file !== null ? `./uploads/publication/${fileName}` : "",
-    
-  //   likers: [],
-    
-  // });
-
-  // try {
-  //   const post = await newPost.save();
-  //   return res.status(201).json(post);
-  // } catch (err) {
-  //   return res.status(400).send(err);
-  // }
+ 
 };
 
 module.exports.updatePost = (req, res) => {
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -16,25 +16,7 @@ module.exports.userInfo = (req, res) => {
   }).select("-password");
 };
 
-// module.exports.updateUser = async (req, res) => {
-//   if (!ObjectID.isValid(req.params.id))
-//     return res.status(400).send("ID unknown : " + req.params.id);
 
-//   try {
-//     await UserModel.findOneAndUpdate(
-//       { _id: req.params.id },
-//       {
-//         $set: {
-//           bio: req.body.bio,
-//         },
-//       },
-//       { new: true, upsert: true, setDefaultsOnInsert: true })
-//       .then((data) => res.send(data))
-//       .catch((err) => res.status(500).send({ message: err }));
-//   } catch (err) {
-//     return res.status(500).json({ message: err });
-//   }
-// };
 
 module.exports.deleteUser = async (req, res) => {
   if (!ObjectID.isValid(req.params.id))
@@ -48,58 +30,5 @@ module.exports.deleteUser = async (req, res) => {
   }
 };
 
-// module.exports.follow = async (req, res) => {
-//   if (
-//     !ObjectID.isValid(req.params.id) ||
-//     !ObjectID.isValid(req.body.idToFollow)
-//   )
-//     return res.status(400).send("ID unknown : " + req.params.id);
 
-//   try {
-//     // add to the follower list
-//     await UserModel.findByIdAndUpdate(
-//       req.params.id,
-//       { $addToSet: { following: req.body.idToFollow } },
-//       { new: true, upsert: true }
-//         .then((data) => res.send(data))
-//         .catch((err) => res.status(500).send({ message: err }))),
-
-//       // add to following list
-//       await UserModel.findByIdAndUpdate(
-//         req.body.idToFollow,
-//         { $addToSet: { followers: req.params.id } },
-//         { new: true, upsert: true }
-//           .then((data) => res.send(data))
-//           .catch((err) => res.status(500).send({ message: err })))
-//   } catch (err) {
-//     return res.status(500).json({ message: err });
-//   }
-// };
-
-// module.exports.unfollow = async (req, res) => {
-//   if (
-//     !ObjectID.isValid(req.params.id) ||
-//     !ObjectID.isValid(req.body.idToUnfollow)
-//   )
-//     return res.status(400).send("ID unknown : " + req.params.id);
-
-//   try {
-//     await userModel.findByIdAndUpdate(
-//       req.params.id,
-//       { $pull: { following: req.body.idToUnfollow } },
-//       { new: true, upsert: true }
-//         .then((data) => res.send(data))
-//         .catch((err) => res.status(500).send({ message: err }))),
-
-//       // Retirer de la liste des followers
-//       await userModel.findByIdAndUpdate(
-//         req.body.idToUnfollow,
-//         { $pull: { followers: req.params.id } },
-//         { new: true, upsert: true }
-//           .then((data) => res.send(data))
-//           .catch((err) => res.status(500).send({ message: err })))
-//   } catch (err) {
-//     return res.status(500).json({ message: err });
-//   }
-// }
 
